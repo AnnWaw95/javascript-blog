f89912daa302e57d480be2a057bd49d02227f635
Ania Wawerek <wawerekanna@gmail.com>
2022-02-24 17:05:14 +0100

    5.3 Add console log for clickedElement

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
 'use strict';
 
 function titleClickHandler(event){
-  console.log('Link was clicked!', event);
+    const clickedElement = this;
+    console.log('Link was clicked!', event);
   
   /* remove class 'active' from all article links  */
-  const activeLinks = document.querySelectorAll('.titles a.active');
+    const activeLinks = document.querySelectorAll('.titles a.active');
 
-  for(let activeLink of activeLinks){
+    for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
   /* add class 'active' to the clicked link */
-
+    clickedElement.classList.add('active');
+    console.log('clickedElement:', clickedElement);
   /* remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.active');
+    const activeArticles = document.querySelectorAll('.active');
 
-  for(let activeArticle of activeArticles){
+    for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
   /* get 'href' attribute from the clicked link */
