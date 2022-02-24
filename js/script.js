08d6003ace6c12619fbd431b0f83e81da9b9bb89
Ania Wawerek <wawerekanna@gmail.com>
2022-02-24 18:25:00 +0100

    Add script

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ function titleClickHandler(event){
   
   /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
-
     for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
@@ -15,21 +14,20 @@ function titleClickHandler(event){
     clickedElement.classList.add('active');
     console.log('clickedElement:', clickedElement);
   /* remove class 'active' from all articles */
-    const activeArticles = document.querySelectorAll('.active');
-
+    const activeArticles = document.querySelectorAll('active');
     for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
   /* get 'href' attribute from the clicked link */
     const articleSelector = clickedElement.getAttribute('href');
-    console.log('articleSelector:', articleSelector);
+    console.log('articleSelector', articleSelector);
   /* find the correct article using the selector (value of 'href' attribute) */
-    const targetAttribute = querySelector('href');
-    console.log('targetAttribute:', targetAttribute);
+    const targetAttribute = document.querySelector('href');
+    console.log('targetAttribut:', targetAttribute);
   /* add class 'active' to the correct article */
-    
-    const targetArticle.classList.add('active');
-    console.log('targetArticle:', targetArticle);
+    const targetArticle = document.querySelector('articleSelector');
+    targetArticle.classList.add('active');
+    console.log('targetArticle', targetArticle);
 }
 
 const links = document.querySelectorAll('.titles a');
